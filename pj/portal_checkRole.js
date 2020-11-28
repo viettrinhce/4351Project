34548171baba0876613d691b082116f07773c7d2
viettrinhce <viettrinh.ce@gmail.com>
2020-11-27 21:44:46 -0600

    newest

--- pj/portal_checkRole.js
+++ pj/portal_checkRole.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
     var sRole = localStorage.getItem("role");
     var aRole = sRole.split("-");
     console.log(aRole.length);
+    console.log(aRole);
+
     for(key in aRole){
-        if (aRole[key] == "hr") {
-            
-        };
         switch(aRole[key]) {
             case "sp":
+              console.log("what is going on");
               jQuery("#portal_support").css("display", "block");
               break;
             case "fn":
